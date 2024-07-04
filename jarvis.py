5b9b815e1dd62b8aad41c5d30d8f9679f2985785
DeveloperAditya <161221864+UndeAditya@users.noreply.github.com>
2024-07-04 20:36:51 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -19,33 +19,30 @@ def processCommand(command):
         webbrowser.open("https://facebook.com")
     elif "open youtube" in command.lower():
         webbrowser.open("https://youtube.com")
-    elif "open linkedin" in command.lower():
-        webbrowser.open("https://linkedin.com")
     elif command.lower().startswith("play"):
         song = command.lower().split(" ")[1]
-        link = musicLibrary.music[song]
-        webbrowser.open(link)
+        if song in musicLibrary.music:
+            link = musicLibrary.music[song]
+            webbrowser.open(link)
+        else:
+            speak(f"Sorry, I couldn't find the song {song}.")
 
 if __name__ == "__main__":
     speak("Initializing Jarvis....")
     while True:
-        # Listen for the wake word "Jarvis"
-        # obtain audio from the microphone
-        r = sr.Recognizer()
-        
         print("recognizing...")
         try:
             with sr.Microphone() as source:
                 print("Listening...")
-                audio = r.listen(source, timeout=2, phrase_time_limit=1)
-            word = r.recognize_google(audio)
-            if(word.lower() == "jarvis"):
+                audio = recognizer.listen(source, timeout=2, phrase_time_limit=1)
+            word = recognizer.recognize_google(audio)
+            if word.lower() == "jarvis":
                 speak("Yes Sir")
                 # Listen for command
                 with sr.Microphone() as source:
                     print("Jarvis Active...")
-                    audio = r.listen(source)
-                    command = r.recognize_google(audio)
+                    audio = recognizer.listen(source)
+                    command = recognizer.recognize_google(audio)
                     processCommand(command)
         except Exception as e:
-            print("Error: {0}".format(e))
+            print(f"Error: {e}")
