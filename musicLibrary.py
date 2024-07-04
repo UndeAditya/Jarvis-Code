bc166a708e14721e91c6c513c8fbff45c769469d
DeveloperAditya <161221864+UndeAditya@users.noreply.github.com>
2024-07-04 20:31:08 +0530

    Create musicLibrary.py

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+music ={
+     
+     "Heat Waves": "https://youtu.be/mRD0-GxqHVo?si=vHVrtNCzHdOQwK8k",
+     "Arijit":  "https://youtu.be/ElZfdU54Cp8?si=fm7_aECHc7FmkYAx",
+     "Charlie": "https://youtu.be/3AtDnEC4zak?si=9b69OPMH6jVF02K0",
+     "Justin Bieber": "https://youtu.be/Fp8msa5uYsc?si=XARaDF6y5uXYidcd",
+     "Skyfall": "https://youtu.be/DeumyOzKqgI?si=NdLAn2ehHtOxrKHi"
+}
